e91f0d2cfbaf7fdb42c56763c36dd9902896160c
bianco9 <lbiancoh@ucenfotec.ac.cr>
2017-06-28 11:44:11 -0600

    Sidenav style-structure

--- src/components/admin/admin.controller.js
+++ src/components/admin/admin.controller.js
@@ -1,4 +1,36 @@
 angular.module('app')
   .controller('adminCtrl', ['$scope', function ($scope) {
-   
+  	var originatorEv;
+	  	/*Sidenav functionality*/
+ 	var originatorEv;
+
+    $scope.openMenu = function($mdMenu, ev) {
+      originatorEv = ev;
+      $mdMenu.open(ev);
+    };
+
+    $scope.notificationsEnabled = true;
+    $scope.toggleNotifications = function() {
+      $scope.notificationsEnabled = !this.notificationsEnabled;
+    };
+
+    $scope.redial = function() {
+      $mdDialog.show(
+        $mdDialog.alert()
+          .targetEvent(originatorEv)
+          .clickOutsideToClose(true)
+          .parent('body')
+          .title('Suddenly, a redial')
+          .textContent('You just called a friend; who told you the most amazing story. Have a cookie!')
+          .ok('That was easy')
+      );
+
+      originatorEv = null;
+    };
+
+    $scope.checkVoicemail = function() {
+      // This never happens.
+    };
+    /*End sidenav functionality
+    -->>*/   
 }]);
