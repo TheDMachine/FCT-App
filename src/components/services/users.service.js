68211349f037b1f8c43115187e31c7e10160c8d9
Josué B <jbrenesr1@ucenfotec.ac.cr>
2017-07-06 22:50:34 -0600

    Comentarios user service
    Se agregan comentario a funciones del user service que no los tenían

--- src/components/services/users.service.js
+++ src/components/services/users.service.js
@@ -72,12 +72,15 @@
       localStorage.setItem('lsUsersList', JSON.stringify(usersList));
     } // todas las funciones que sean llamadas por ajax deben estar debajo del return, para que cuando angular corra el script haga el return y devuelva el api , las funciones debajo del return son privadas y se devuelve el api que es el que contiene las funciones
 
+    //agrega profesores al localStorage
     function _setTeachers(pTeacher) {
       var teachersList = _getTeachers();
 
       teachersList.push(pTeacher);
       localStorage.setItem('lsTeachersList', JSON.stringify(teachersList));
     }
+
+    //muestra los usuarios profesores actualizados
     function _getTeachers() {
       var teachersList = JSON.parse(localStorage.getItem('lsTeachersList'));
       if(teachersList == null){
@@ -85,6 +88,8 @@
       }
       return teachersList;
     }
+
+    //editar la informacion del profesor 
     function _updateTeacher(pobjUsuario) {
       var teachersList = _getTeachers();
       for(var i = 0; i < teachersList.length; i++){
