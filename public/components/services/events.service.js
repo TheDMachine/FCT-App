6ff7a42e39870810ad3383007b44edd8cff8cf44
bianco9 <lbiancoh@ucenfotec.ac.cr>
2017-08-24 12:03:05 -0600

    coma

--- public/components/services/events.service.js
+++ public/components/services/events.service.js
@@ -23,7 +23,7 @@
       findPropose: _findProposeEvent,
       updateProposeToEvent: _updateProposeToEvent,
       updateReject: _updateReject,
-      deleteCompetition: _deleteCompetition
+      deleteCompetition: _deleteCompetition,
       findCompetition: _findCompetition
     };
     return publicAPI; // todas las funciones que sean llamadas por ajax deben estar debajo del return, para que cuando angular corra el script haga el return y devuelva el api , las funciones debajo del return son privadas y se devuelve el api que es el que contiene las funciones
